c1f4338bdf3e63518784dd191cc20dfa2ceed163
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2017-07-19 21:45:30 +0200

    project filles

---
+++ Assets/Scripts/ObjImporter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjImporter : MonoBehaviour {
+
+	public string Path;
+
+	public void ImportObjModel()
+	{
+		Debug.Log ("Initalizing obj model: " + Path);
+		GameObject objGameObject = OBJLoader.LoadOBJFile (Path);
+		Material[] materials = OBJLoader.LoadMTLFile (Path);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
